e243c49805353729a68a0bc9a6ec07eb557cf27c
bhavana desai <bhavanadesai@gmail.com>
2024-07-31 11:25:45 +0530

    Exception demo

---
+++ SBIApp/ExceptionHandlingDemo.cs
@@ -0,0 +1,61 @@
+﻿using BankLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBIApp
+{
+    internal class ExceptionHandlingDemo
+    {
+        static void Main(string[] args)
+        {
+            //try  catch finally throw
+
+            int num1, num2, result;
+            Savings savings ;
+            try
+            {
+                Console.WriteLine("Enter two integer");
+                num1 = int.Parse(Console.ReadLine());
+                num2 = int.Parse(Console.ReadLine());
+                if (num2 < 1)
+                {
+                    throw new DivideByZeroException("message ...");
+                }
+                Console.WriteLine("num1 = " + num1);
+                Console.WriteLine("num2 = " + num2);
+                //
+                //
+                //
+                result = num1 / num2;
+                Console.WriteLine("Result = " + result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide number by zero \nPlease privide positive number");
+                Console.WriteLine(ex.Message);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Provide Positive integer");
+            }
+            catch (Exception ex)
+            {
+               
+                Console.WriteLine(ex.Message);
+            }
+            //finally
+            //{
+            //    Console.WriteLine("Program ends");
+            //}
+            Console.WriteLine("Program ends");
+            Console.ReadLine();
+
+            //15    16  17  21
+
+        }
+    }
+}
---
+++ SBIApp/TestInnerException.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBIApp
+{
+    internal class TestInnerException
+    {
+        static void Main(string[] args)
+        {
+            // throw new ArgumentException();
+
+            try
+            {
+                try
+                {
+                    throw new ArgumentException();
+                }
+
+                catch (ArgumentException e)
+                {
+                    //make sure this path does not exist
+                    if (File.Exists("c:\\test.txt") == false)
+                    {
+                        throw new FileNotFoundException("\nFile Not found when trying to write argument exception to the file", e);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                //The e.StackTrace property gives us the location of the error and has the value 
+
+                Console.WriteLine(String.Concat(e.StackTrace, e.Message));
+
+
+                //check if the InnerException is null or not. If it is not null, we can display InnerException.StackTrace and InnerException.Message
+
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("\nInner Exception\n");
+                    Console.WriteLine(String.Concat(e.InnerException.StackTrace, e.InnerException.Message));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*
+ The InnerException is a property of an exception. 
+
+When there are series of exceptions, the most current exception can obtain the prior exception in the InnerException property.
+
+ the InnerException property contains the actual exception. This is the reason for the existence of an InnerException property for any exception.
+
+Example
+
+Let us say we have an exception inside a try block throwing an ArgumentException and the catch clause catches it and writes it to a file. However, if the file path is not found, FileNotFoundException is thrown. Let's say that the outside try block catches this exception, but how about the actual ArgumentException that was thrown? Is it lost? No, the InnerException property contains the actual exception. This is the reason for the existence of an InnerException property for any exception
+*/
